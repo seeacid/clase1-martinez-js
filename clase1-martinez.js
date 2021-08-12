f72784100b87071e6561ca354c36a2575829fe95
seeacid <seeacid@gmail.com>
2021-08-12 09:13:44 -0300

    clase 1

---
+++ clase1-martinez.js
@@ -0,0 +1,9 @@
+var nombre = prompt("Ingrese su nombre");
+var apellido = prompt("Ingrese su apellido");
+var bienvenida = nombre + " " + apellido;
+alert("Bienvenido " + bienvenida);
+var nacimiento = prompt("Ingrese su año de nacimiento");
+parseInt(nacimiento);
+var edad = 2021 - nacimiento;
+alert("Usted tiene " + edad + " Años");
+alert("Su nombre de usuario es " + nombre + edad);
